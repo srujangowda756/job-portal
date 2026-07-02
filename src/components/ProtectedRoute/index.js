14bbcd8a213b69cdf9698b114ef8cd3fc142726f
Srujan K <srujangowda756@gmail.com>
2026-07-02 16:00:58 +0530

    Comment out unused imports in ProtectedRoute
    Comment out unused imports and code related to authentication.

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,6 +1,6 @@
 import {Route} from 'react-router-dom'
 //import {Route, Redirect} from 'react-router-dom'
-import Cookies from 'js-cookie'
+//import Cookies from 'js-cookie'
 
 const ProtectedRoute = props => {
   //const jwtToken = Cookies.get('jwt_token')
