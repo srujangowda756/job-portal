5f0c9885b3c1212d4e77bd5ecd10b3095ccf63cf
Srujan K <srujangowda756@gmail.com>
2026-07-02 15:59:16 +0530

    Refactor ProtectedRoute component
    Comment out unused imports and jwtToken check.

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,8 +1,9 @@
-import {Route, Redirect} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+//import {Route, Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 const ProtectedRoute = props => {
-  const jwtToken = Cookies.get('jwt_token')
+  //const jwtToken = Cookies.get('jwt_token')
   //if (jwtToken === undefined) return <Redirect to="/login" />
   return <Route {...props} />
 }
